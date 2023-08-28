208920e87b1fbd712c9a577d404e4914a887d89b
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-28 13:42:14 +0300

    Task solved

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,24 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
 
+            // Print decreasing numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            // Print increasing numbers
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println(); // Move to the next line for the next row
+        }
     }
 
     public static void main(String[] args) {
